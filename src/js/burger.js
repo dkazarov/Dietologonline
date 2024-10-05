9c07ee0801a779b6118c25a217c947652d0df9e7
unknown <d.kazarov1361@gmail.com>
2024-10-05 22:36:34 +0300

    add hero adaptive max-width: 768px + fix header menu + fix scroll menu + some fix

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,12 +2,14 @@ const burgerButton = document.querySelector('.burger');
 const burgerLines = document.querySelector('.burger__line');
 const headerNav = document.querySelector('.header__nav');
 const navItems = document.querySelectorAll('.header-nav__item');
+const body = document.body;
 
 const burger = () => {
 	burgerButton.addEventListener('click', () => {
 		burgerLines.classList.toggle('burger--transform');
 		burgerButton.classList.toggle('burger--transform');
 		headerNav.classList.toggle('header__nav--toogle');
+		body.classList.add('disable-scroll');
 	});
 
 	navItems.forEach(elem => {
