28ef48249a12d992b17acd352da08cf0ac20a949
unknown <d.kazarov1361@gmail.com>
2024-10-05 19:36:02 +0300

    add js func to close header menu after click

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,16 @@
 const burgerButton = document.querySelector('.burger');
 const burgerLines = document.querySelector('.burger__line');
 const headerNav = document.querySelector('.header__nav');
+const navItems = document.querySelectorAll('.header-nav__item');
 
 burgerButton.addEventListener('click', () => {
 	burgerLines.classList.toggle('burger--transform');
 	burgerButton.classList.toggle('burger--transform');
 	headerNav.classList.toggle('header__nav--toogle');
+});
+
+navItems.forEach(elem => {
+	elem.addEventListener('click', () => {
+		headerNav.classList.remove('header__nav--toogle');
+	});
 });
