3e53e7e3bac63f5f031745c376a5feed308bc97a
unknown <d.kazarov1361@gmail.com>
2024-10-08 21:52:57 +0300

    add acordion to adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,21 @@
 import burger from './burger';
 
 burger();
+
+const card = document.querySelectorAll('.problems-card');
+
+const acoordeonOn = () => {
+	card.forEach(elem => {
+		elem.addEventListener('click', event => {
+			const content = document.querySelector('.problems-card__content');
+
+			const current = event.currentTarget;
+			current.classList.toggle('active');
+			content.style = content.scrollHeight + 'px';
+
+			// current.classList.remove('active');
+		});
+	});
+};
+
+acoordeonOn();
